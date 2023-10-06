3db693887bce0dee9940dbfd0d30f411acd8a828
AdmiralCringe <Sasharazzhivin@yandex.ru>
2023-10-06 12:25:11 +0300

    fix Task2

--- Task2.js
+++ Task2.js
@@ -1,28 +1,28 @@
 function checkSyntax(string) {
-    const openBrackets = ['(', '[', '{', '<'];
-    const closeBrackets = [')', ']', '}', '>'];
-    const stack = [];
-  
-    for (let i = 0; i < string.length; i++) {
-      const currentChar = string[i];
-  
-      if (openBrackets.includes(currentChar)) {
-        stack.push(currentChar);
-      }
-  
-      if (closeBrackets.includes(currentChar)) {
-        const matchingOpeningBracket = openBrackets[closeBrackets.indexOf(currentChar)];
-  
-        if (stack.length === 0 || stack[stack.length - 1] !== matchingOpeningBracket) {
-          return 1;
-        }
-  
-        stack.pop();
-      }
+  const openBrackets = ['<', '[', '{', '('];
+  const closeBrackets = ['>', ']', '}', ')'];
+  const stack = [];
+
+  for (let i = 0; i < string.length; i++) {
+    const currentChar = string[i];
+
+    if (openBrackets.includes(currentChar)) {
+      stack.push(currentChar);
     }
-  
-    return stack.length === 0 ? 0 : 1;
+
+    if (closeBrackets.includes(currentChar)) {
+      const matchingOpeningBracket = openBrackets[closeBrackets.indexOf(currentChar)];
+
+      if (stack.length === 0 || stack[stack.length - 1] !== matchingOpeningBracket) {
+        return 1;
+      }
+
+      stack.pop();
+    }
   }
+
+  return stack.length === 0 ? 0 : 1;
+}
   console.log(checkSyntax('---(++++)----'))
   console.log(checkSyntax(' '))
   console.log(checkSyntax('before ( middle []) after '))
@@ -31,4 +31,6 @@ function checkSyntax(string) {
   console.log(checkSyntax('<(   >)'))
   console.log(checkSyntax('(  [  <>  ()  ]  <>  )'))
   console.log(checkSyntax('   (      [)'))
+
+  // В случае изменения "набор скобок: <,[,{" будет меняться массивы скобок
   
